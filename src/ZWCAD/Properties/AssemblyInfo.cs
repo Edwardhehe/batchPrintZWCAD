57d093df844e65c717ea678fc10cad0f32124b46
Edward <lihaoyangyan@163.com>
2026-09-26 20:09:40 +0800

    chore: 发布 v1.15.7.7
    相对 v1.15.7.6：打印对象线宽改用 CTB 对象线宽临时副本，多文件文件夹批打与空间快速勾选，UCS 模型不再打斜，修复合并 PDF 单张卡住与进度显示，模型拆图改为 Wblock + XClip，去掉右键提示文字。

--- src/ZWCAD/Properties/AssemblyInfo.cs
+++ src/ZWCAD/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.15.7.6")]
-[assembly: AssemblyFileVersion("1.15.7.6")]
-[assembly: AssemblyInformationalVersion("1.15.7.6")]
+[assembly: AssemblyVersion("1.15.7.7")]
+[assembly: AssemblyFileVersion("1.15.7.7")]
+[assembly: AssemblyInformationalVersion("1.15.7.7")]
